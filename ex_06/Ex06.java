a723d2f29087b5f7fd54a691e6b5fa6e8e018ca4
xRezio <edwin.carin@epitech.eu>
2023-10-23 11:07:51 +0200

    ajout de l'exercice 6

---
+++ ex_06/Ex06.java
@@ -0,0 +1,38 @@
+public class Ex06 {
+    public static void sequence(int nbr) {
+        if (nbr < 0) {
+            return;
+        }
+
+        String term = "1";
+        System.out.println(term);
+
+        for (int i = 1; i < nbr; i++) {
+            term = getNextTerm(term);
+            System.out.println(term);
+        }
+    }
+
+    private static String getNextTerm(String term) {
+        StringBuilder nextTerm = new StringBuilder();
+        char[] chars = term.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            int count = 1;
+            while (i + 1 < chars.length && chars[i] == chars[i + 1]) {
+                i++;
+                count++;
+            }
+            nextTerm.append(count).append(chars[i]);
+        }
+
+        return nextTerm.toString();
+    }
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+                int nbr = Integer.parseInt(args[0]);
+                sequence(nbr);
+    }
+}
+}
