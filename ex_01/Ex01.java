91b9695ca6daef4ef2f037a8a105d7e60ad3c561
xRezio <edwin.carin@epitech.eu>
2023-10-23 10:14:42 +0200

    ajout de l'exercice 1

---
+++ ex_01/Ex01.java
@@ -0,0 +1,7 @@
+    public static void main(String[] args) {
+        myConcat("Hello", "World!");
+    }
+
+    public static void myConcat(String str1, String str2) {
+        System.out.print(str1 + " " + str2);
+    }
