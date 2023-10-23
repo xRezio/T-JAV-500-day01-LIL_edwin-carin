9fc1f6a3ed806100723e333ecf00d1bb86c323d5
xRezio <edwin.carin@epitech.eu>
2023-10-23 11:51:05 +0200

    modification exo 3 - une seule boucle for

--- ex_03/Ex03.java
+++ ex_03/Ex03.java
@@ -1,18 +1,11 @@
 import java.util.ArrayList;
 
 public class Ex03 {
-    public static void printArray(ArrayList<String> myArray) {
-        for (String element : myArray) {
-            System.out.println(element);
-        }
-    }
-
     public static void main(String[] args) {
         ArrayList<String> carList = new ArrayList<>();
         for (String arg : args) {
             carList.add(arg);
+            System.out.println(arg);
         }
-
-        Ex03.printArray(carList);
     }
 }
